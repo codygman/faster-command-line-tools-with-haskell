0bf43754acfcffb3119088decbfaebd24fef093e
Cody Goodman <codygman.consulting@gmail.com>
2017-07-31 00:00:07 -0500

    add fast strict haskell version

--- haskell-version/src/Main.hs
+++ haskell-version/src/Main.hs
@@ -1,7 +1,23 @@
 module Main where
 
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.IntMap.Strict as M
+import Data.List
+import Data.ByteString.Lex.Integral
+
+toInt :: C8.ByteString -> Int
+toInt = readDecimal_
+
+processFile = do
+  bsLines <- C8.lines <$> C8.readFile "../ngrams.tsv"
+  let info = C8.splitWith (=='\t') <$> bsLines :: [[C8.ByteString]]
+  let m = foldl'
+             (\acc x -> let (_:key:val:_) = (x :: [C8.ByteString]) in M.insertWith (\new old -> old + new) (toInt key) (toInt val) acc)
+             M.empty
+             info
+  pure $ last $ sortOn snd (M.toList m)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  x <- processFile
+  print x
