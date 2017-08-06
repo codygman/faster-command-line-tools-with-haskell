29e50ea67c1cfb5c8d9f71b600ad5a15e12b3821
Cody Goodman <codygman.consulting@gmail.com>
2017-08-06 02:59:59 -0500

    use custom mywords

--- haskell-version/src/Main.hs
+++ haskell-version/src/Main.hs
@@ -8,19 +8,32 @@ import qualified Data.ByteString.Char8 as BS
 import Data.ByteString.Lex.Integral
 import Control.Monad.Primitive
 import Control.Monad (forM_)
+import Data.Word (Word8)
+import qualified Data.ByteString as B
 
-processFile :: FilePath -> IO (VM.MVector RealWorld Int)
-processFile path = do
-  content <- BS.readFile path
+isSpaceWord8 :: Word8 -> Bool
+isSpaceWord8 !w = w == 0x09 -- \t
+{-# INLINE isSpaceWord8 #-}
+
+mywords :: BS.ByteString -> [BS.ByteString]
+mywords bs = B.splitWith isSpaceWord8 bs
+{-# INLINE mywords #-}
+
+processFile :: BS.ByteString -> IO (VM.MVector RealWorld Int)
+processFile content = do
   vec <- VGM.new 2009
-  forM_ (BS.split '\t' <$> BS.lines content) $ \line -> do
-    let (_ : k : v : _) = readDecimal_ <$> line
-    VGM.modify vec (+v) k
+  forM_ (mywords <$> BS.lines content) $ \line -> do
+    let (_ : k : v : _) = line
+        keyInt = readDecimal_ k
+        valInt = readDecimal_ v
+    VGM.unsafeModify vec (+ valInt) keyInt
   return vec
+{-# INLINE processFile #-}
 
 main :: IO ()
 main = do
-  vec <- processFile "../ngrams.tsv"
-  vec' <- VU.freeze vec
+  content <- BS.readFile "../ngrams.tsv"
+  vec <- processFile content
+  vec' <- VU.unsafeFreeze vec
   let answer = VU.maxIndex vec'
   print answer
