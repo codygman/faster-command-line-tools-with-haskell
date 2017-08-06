c95d0fa778b0cfd82be2b58d65b71379ffe15660
Cody Goodman <codygman.consulting@gmail.com>
2017-08-06 01:29:23 -0500

    low level version

--- haskell-version/src/Main.hs
+++ haskell-version/src/Main.hs
@@ -1,17 +1,26 @@
+{-# Language BangPatterns #-}
 module Main where
 
-import qualified Data.ByteString.Char8 as C8
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Generic.Mutable as VGM
+import qualified Data.Vector.Unboxed.Mutable as VM
+import qualified Data.ByteString.Char8 as BS
 import Data.ByteString.Lex.Integral
-import qualified Data.Vector.Unboxed as V
+import Control.Monad.Primitive
+import Control.Monad (forM_)
 
-toInt :: C8.ByteString -> Int
-toInt = readDecimal_
+processFile :: FilePath -> IO (VM.MVector RealWorld Int)
+processFile path = do
+  content <- BS.readFile path
+  vec <- VGM.new 2009
+  forM_ (BS.split '\t' <$> BS.lines content) $ \line -> do
+    let (_ : k : v : _) = readDecimal_ <$> line
+    VGM.modify vec (+v) k
+  return vec
 
-{-# INLINE process #-}
-process bs = (i, xs V.! i) where
-  info = C8.splitWith (=='\t') <$> C8.lines bs
-  xs = V.unsafeAccumulate (+) (V.replicate 2009 0) . V.fromList
-     $ map (\(_:key:val:_) -> (toInt key, toInt val)) info
-  i = V.maxIndex xs
-
-main = print . process =<< C8.readFile "../ngrams.tsv"
+main :: IO ()
+main = do
+  vec <- processFile "../ngrams.tsv"
+  vec' <- VU.freeze vec
+  let answer = VU.maxIndex vec'
+  print answer
