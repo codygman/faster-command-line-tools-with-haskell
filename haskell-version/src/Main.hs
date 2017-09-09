020b75b01d2b78c32491ed0867b1d4de4dcf33d2
Weerasak Chongnguluam <singpor@gmail.com>
2017-09-09 22:43:07 +0700

    Use foldl for convert to Int

--- haskell-version/src/Main.hs
+++ haskell-version/src/Main.hs
@@ -1,24 +1,30 @@
-{-# Language BangPatterns #-}
 module Main where
 
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as VM
 import qualified Data.ByteString.Char8 as BS
-import Data.ByteString.Lex.Integral
 import Control.Monad (forM_)
+import Data.Char (ord)
+
+toInt :: BS.ByteString -> Int
+toInt = BS.foldl (\n c -> n*10 + ord c - 48) 0
+
+parseKeyValue :: BS.ByteString -> (Int, Int)
+parseKeyValue b = let
+    rest          = BS.drop 1 $ BS.dropWhile (/= '\t') b
+    (k, rest')    = BS.break (== '\t') rest
+    (v, _ )       = BS.break (== '\t') $ BS.drop 1 rest'
+  in
+    (toInt k, toInt v)
+{-# INLINE parseKeyValue #-}
 
 processFile :: BS.ByteString -> IO (VM.IOVector Int)
 processFile content = do
   vec <- VM.new 2009
   forM_ (BS.lines content) $ \line -> do
-    let line' = BS.drop 1 $ BS.dropWhile (/= '\t') line
-    let (k, line'') = BS.break (== '\t') line'
-    let (v, _ ) = BS.break (== '\t') $ BS.drop 1 line''
-    let keyInt = readDecimal_ k
-    let valInt = readDecimal_ v
-    VM.unsafeModify vec (+ valInt) keyInt
-  return $! vec
-{-# INLINE processFile #-}
+    let (key, val) = parseKeyValue line
+    VM.unsafeModify vec (+ val) key
+  return vec
 
 main :: IO ()
 main = do
